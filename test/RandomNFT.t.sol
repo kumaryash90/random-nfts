8a7a0b51f898255d86cd1ffa9f1a2b8052ca7657
yash <kumaryashcse@gmail.com>
2022-05-09 14:04:54 +0530

    more unit tests

--- test/RandomNFT.t.sol
+++ test/RandomNFT.t.sol
@@ -57,8 +57,63 @@ contract RandomNFTTest is Test {
         assertEq(balance, 3);
     }
 
-    function testWithdrawalWorksAsOwner() public {
+    function safeContractReceiver() public {
+        Receiver r = new Receiver();
+        rnft.mintTo{value: 0.001 ether}(address(r));
 
+        uint256 slot = stdstore.target(address(rnft)).sig(rnft.balanceOf.selector).with_key(address(r)).find();
+        bytes32 loc = bytes32(slot);
+        uint256 balance = uint256(vm.load(address(rnft), loc));
+
+        assertEq(balance, 1);
+    }
+
+    function testFailUnsafeContractReceiver() public {
+        vm.etch(address(25), bytes("akjdnckjsdcs"));
+        rnft.mintTo{value: 0.001 ether}(address(25));
+    }
+
+    function testWithdrawalWorksAsOwner() public {
+        rnft.mintTo{value: 0.001 ether}(address(12));
+
+        uint256 rnftBalance = address(rnft).balance;
+        
+        rnft.withdraw(payable(address(27)));
+        uint256 payeeBalance = address(27).balance;
+        assertEq(payeeBalance, rnftBalance);
+
+        rnftBalance = address(rnft).balance;
+        assertEq(rnftBalance, 0);
+    }
+
+    function testFailNotOwner() public {
+        rnft.mintTo{value: 0.001 ether}(address(12));
+
+        uint256 rnftBalance = address(rnft).balance;
+        
+        vm.startPrank(address(1));
+        rnft.withdraw(payable(address(27)));
+        vm.stopPrank();
+    }
+
+    function testTokenURI() public {
+        rnft.mintTo{value: 0.001 ether}(address(1));
+        string memory uri = rnft.tokenURI(1);
+        string memory baseUri = rnft.baseURI();
+
+        assertEq(uri, string(abi.encodePacked(baseUri, "1")));
+    }
+
+    function testFailTokenNonExistent() public {
+        rnft.mintTo{value: 0.001 ether}(address(1));
+        rnft.tokenURI(2);
+    }
+
+    function testNextTokenId() public {
+        uint256 tokenId = rnft.mintTo{value: 0.001 ether}(address(12));
+        uint256 nextTokenId = rnft.nextTokenId();
+
+        assertEq(tokenId, nextTokenId);
     }
 }
 
